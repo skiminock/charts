c245e1fbfae5d54b2a1aa43df345681cedb11c4c
Чепурной Александр Владимирович <chepurnoy.a.v@sberbank.ru>
2020-05-13 00:18:59 +0300

    update

--- a/script.js
+++ a/script.js
@@ -1,87 +1,102 @@
-const height = 500
-const width = 954
-const margin = { top: 20, right: 0, bottom: 30, left: 40 }
+const height = 248
+const width = 544
+const margin = { top: 20, right: 40, bottom: 30, left: 40 }
 const extent = [[margin.left, margin.top], [width - margin.right, height - margin.top]];
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
 
     const data = [
-        { name: '1', value: 1 },
-        { name: '2', value: 2 },
-        { name: '3', value: 3 },
-        { name: '4', value: 4 },
-        { name: '5', value: 5 },
-        { name: '6', value: 6 },
-        { name: '7', value: 7 },
-        { name: '8', value: 8 },
-        { name: '9', value: 9 },
-        { name: '10', value: 10 },
-
-        { name: '11', value: 11 },
-        { name: '12', value: 2 },
-        { name: '13', value: 3 },
-        { name: '14', value: 4 },
-        { name: '15', value: 5 },
-        { name: '16', value: 6 },
-        { name: '17', value: 7 },
-        { name: '18', value: 8 },
-        { name: '19', value: 9 },
-        { name: '20', value: 10 },
+        { year: '2019', value: 1 },
+        { year: '2020', value: 2 },
+        { year: '2021', value: 4 },
+        { year: '2022', value: 6 },
+        { year: '2023', value: 7 },
+        { year: '2024', value: 8 },
+        { year: '2025', value: 9 },
+        { year: '2026', value: 10 },
+        { year: '2027', value: 12 },
+        { year: '2028', value: 15 }
     ]
 
-    const x = d3.scaleBand()
-        .domain(data.map(d => d.name))
-        .range([margin.left, width - margin.right])
-        .padding(0.1)
+    const barWidth = 24
+    const barSpace = 40;
+    const xScaleRange = data.length * (barWidth + barSpace);
+    const barsRange = [margin.left, xScaleRange + margin.right]
 
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.value)]).nice()
+    const x = d3
+        .scaleBand()
+        .domain(data.map(d => d.year))
+        .range(barsRange)
+        .padding(0)
+
+    const y = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.value)])
+        .nice()
         .range([height - margin.bottom, margin.top])
 
     const xAxis = g => g
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0))
+        .attr('transform', `translate(0,${height - margin.bottom})`)
+        .call(
+            d3
+                .axisBottom(x)
+                .tickSizeOuter(0)
+        )
 
     const yAxis = g => g
-        .attr("transform", `translate(${margin.left},0)`)
+        .attr('transform', `translate(${margin.left},0)`)
         .call(d3.axisLeft(y))
-        .call(g => g.select(".domain").remove())
-
-    d3.create("svg")
-        .attr("viewBox", [0, 0, width, height])
+        //.call(g => g.select('.domain').remove())
 
     d3.select('svg')
-        .append("g")
-        .attr("class", "bars")
-        .attr("fill", "#33CCFF")
-        .selectAll("rect")
-        .data(data)
-        .join("rect")
-        .attr("x", d => x(d.name))
-        .attr("y", d => y(d.value))
-        .attr("height", d => y(0) - y(d.value))
-        .attr("width", x.bandwidth());
+        .attr('width', width)
+        .attr('height', height)
+        .attr('viewBox', [0, 0, width, height])
 
-    d3.select('svg').append("g")
-        .attr("class", "x-axis")
+    d3.select('svg')
+        .append('g')
+        .attr('class', 'bars')
+        .attr('fill', '#33CCFF')
+        .selectAll('rect')
+        .data(data)
+        .join('rect')
+        .attr('x', d => x(d.year)  + (barSpace / 2))
+        .attr('y', d => y(d.value))
+        .attr('width', x.bandwidth() - barSpace)
+        .attr('height', d => y(0) - y(d.value));
+
+    d3.select('svg').append('g')
+        .attr('class', 'x-axis')
         .call(xAxis);
 
-    d3.select('svg').append("g")
-        .attr("class", "y-axis")
+    d3.select('svg').append('g')
+        .attr('class', 'y-axis')
         .call(yAxis);
 
     d3.select('svg')
-        .call(d3.zoom()
-            .extent(extent)
-            .scaleExtent([1, 1])
-            //.translateExtent(extent)
-            .on("zoom", () => { zoomed() })
+        .call(
+            d3
+                .zoom()
+                .extent(extent)
+                .scaleExtent([1, 1])
+                //.translateExtent(extent)
+                .on('zoom', zoomed)
         )
 
     function zoomed() {
-        x.range([margin.left, width - margin.right].map(d => d3.event.transform.applyX(d)));
-        d3.select('svg').selectAll(".bars rect").attr("x", d => x(d.name)).attr("width", x.bandwidth());
-        d3.select('svg').selectAll(".x-axis").call(xAxis);
+
+        x.range(barsRange.map(d => d3.event.transform.applyX(d)));
+
+        d3
+            .select('svg')
+            .selectAll('.bars rect')
+            .attr('x', d => x(d.year) + (barSpace / 2))
+            .attr('width', x.bandwidth() - barSpace);
+
+        d3
+            .select('svg')
+            .selectAll('.x-axis')
+            .call(xAxis);
     }
 
 })
